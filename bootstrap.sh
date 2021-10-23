6245b79f3da85dde4fe0efb7daa9257c700ad89e
Aleksey Pedan <35327043+alexspedan@users.noreply.github.com>
2021-10-23 22:48:19 +0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,18 +1,21 @@
 cat <<EOT > bootstrap.sh
 #!/bin/sh
 
-sudo apt upgrade && sudo apt update
-sudo apt install default-jre -y
-sudo apt install git -y
-sudo apt install nodejs -y
-sudo apt install openjdk-11-jre-headless -y
+sudo -i
+echo 'nameserver 8.8.8.8' > /etc/resolv.conf
+echo 'options edns0' >> /etc/resolv.conf
+apt upgrade && sudo apt update
+apt install default-jre -y
+apt install git -y
+apt install nodejs -y
+apt install openjdk-11-jre-headless -y
 mkdir backend
 git clone https://github.com/alexspedan/chapter.git
 cd /home/vagrant/chapter/frontend
-sudo npm install
-sudo npm run build
-sudo npm install -g http-server
-sudo http-server /home/vagrant/frontend -p 80 --proxy http://localhost:8080
+npm install
+npm run build
+npm install -g http-server
+http-server /home/vagrant/frontend -p 80 --proxy http://localhost:8080
 cd
 cd /home/vagrant/chapter
 java -jar sausage-store-0.0.1.jar
